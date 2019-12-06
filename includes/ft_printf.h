0ebe58b68ce4f5e09a46ee046c1af4a2a6da52a7
Dylan Gascon <dgascon@z4r8p6.le-101.fr>
2019-12-06 05:17:50 +0100

    Add lib stdarg

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: dgascon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/20 05:36:15 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/06 05:16:27 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/06 05:17:18 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
+# include <stdarg.h>
 #endif
