4c4b4ca8d6312ffea283e4483dae1b6eb0871b59
Dylan Gascon <dgascon@z4r8p6.le-101.fr>
2019-12-06 05:19:08 +0100

    Add my lib libft

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dgascon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/20 05:36:15 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/06 05:18:02 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/06 05:18:47 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,4 +15,5 @@
 # define FT_PRINTF_H
 # include <stdio.h>
 # include <stdarg.h>
+# include "../libft/includes/libft.h"
 #endif
